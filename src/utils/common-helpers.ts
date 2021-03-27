665af9313481c1e50573a08da228ff7103f46bae
Suchita Doshi <suchitadoshi1987@gmail.com>
2021-03-26 17:57:46 -0700

    add fallback for appRoot

--- src/utils/common-helpers.ts
+++ src/utils/common-helpers.ts
@@ -2,7 +2,7 @@ import { Server } from '..';
 
 export async function getAppRootFromConfig(server: Server) {
   try {
-    return await server.connection.workspace.getConfiguration('els.appRoot');
+    return (await server.connection.workspace.getConfiguration('els.appRoot')) || Promise.resolve('');
   } catch (e) {
     return Promise.resolve('');
   }
